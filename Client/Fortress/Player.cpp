812fdaa6aafd81d05e4d5b2ad6e5e1ae8ad62aed
sinoh19 <60386845+sinoh19@users.noreply.github.com>
2025-11-19 10:27:52 +0900

    player 수정
    cpp, h 수정

--- Client/Fortress/Player.cpp
+++ Client/Fortress/Player.cpp
@@ -1,10 +1,9 @@
 #include "Player.h"
 
-#ifndef PLAYER_H
-#define PLAYER_H
+// ========================== Player ===========================
 
 Player::Player()
-    :left(0)
+    : left(0)
     , top(0)
     , right(0)
     , speed(10)
@@ -22,7 +21,9 @@ Player::Player()
     , isleftUp(false)
     , HP(100)
     , HP_MAX(100)
-    , power_now(0){}
+    , power_now(0)
+{
+}
 
 void Player::set_pos(int p1_left, int p1_top)
 {
@@ -41,8 +42,9 @@ void Player::Space_Down()
 
 void Player::Move(bool isFire, int tank_mode)
 {
-    if (isFire == true)
+    if (isFire)
         return;
+
     if (GetAsyncKeyState(VK_LEFT))
     {
         if (tank_mode == 1)
@@ -61,6 +63,7 @@ void Player::Move(bool isFire, int tank_mode)
                 left -= 4;
         }
     }
+
     if (GetAsyncKeyState(VK_RIGHT))
     {
         if (tank_mode == 1)
@@ -90,6 +93,7 @@ void Player::Charge()
 {
     if (power < power_Max)
         power += power_Max * 0.01;
+
     if (power_now < power_Max)
         power_now += power_Max * 0.01;
 }
@@ -103,43 +107,52 @@ void Player::SpaceUp(HWND hWnd)
 {
     isSpaceUp = true;
     power_now = power;
-    //SetTimer(hWnd, 6, 60, NULL); // 탱크 1
-    //SetTimer(hWnd, 7, 60, NULL); // 탱크 2
-    //SetTimer(hWnd, 8, 60, NULL); // 탱크 3
+
+    // 발사 애니메이션 타이머는 WinMain 쪽에서 관리
+    // SetTimer(hWnd, 6, 60, NULL); // 탱크 1
+    // SetTimer(hWnd, 7, 60, NULL); // 탱크 2
+    // SetTimer(hWnd, 8, 60, NULL); // 탱크 3
 }
 
 void Player::Update(bool isFire, HWND hWnd)
 {
-    if (isFire == true)
+    if (isFire)
         return;
+
+    // 스페이스(발사 버튼) 처리
     if (GetAsyncKeyState(VK_SPACE))
     {
-        if (isSpacePress == false) {
+        if (!isSpacePress)
+        {
             isSpaceDown = true;
             isSpacePress = true;
         }
-        if (isSpaceDown == true)
+
+        if (isSpaceDown)
         {
             Charge();
         }
     }
     else
     {
-        if (isSpacePress == true && isSpaceDown == true)
+        if (isSpacePress && isSpaceDown)
         {
             isSpaceDown = false;
             isSpacePress = false;
             SpaceUp(hWnd);
         }
     }
-    // ---
+
+    // 좌우 이동 키에 따른 속도 게이지
     if (GetAsyncKeyState(VK_LEFT) || GetAsyncKeyState(VK_RIGHT))
     {
-        if (isleftPress == false) {
+        if (!isleftPress)
+        {
             isleftDown = true;
             isleftPress = true;
         }
-        if (isleftDown == true)
+
+        if (isleftDown)
         {
             if (Speed < Speed_Max)
                 Speed += Speed_Max * 0.02;
@@ -147,7 +160,7 @@ void Player::Update(bool isFire, HWND hWnd)
     }
     else
     {
-        if (isleftPress == true && isleftDown == true)
+        if (isleftPress && isleftDown)
         {
             isleftDown = false;
             isleftPress = false;
@@ -157,73 +170,91 @@ void Player::Update(bool isFire, HWND hWnd)
 
 void Player::Render_PowerGauge(HDC hdc, int camera_x, int camera_y)
 {
-    // 파워 게이지를 화면에 표시
-    int gaugeWidth = 265; // 게이지의 전체 너비
-    int gaugeHeight = 13; // 게이지의 높이
-    int filledWidth = static_cast<int>((power_now / power_Max) * gaugeWidth); // 충전된 게이지의 너비
+    const int gaugeWidth = 265;
+    const int gaugeHeight = 13;
+
+    int filledWidth = static_cast<int>((power_now / power_Max) * gaugeWidth);
 
     // 게이지 배경
-    Rectangle(hdc, camera_x + 224, camera_y + 368, camera_x + 224 + gaugeWidth, camera_y + 368 + gaugeHeight);
+    Rectangle(hdc,
+        camera_x + 224, camera_y + 368,
+        camera_x + 224 + gaugeWidth, camera_y + 368 + gaugeHeight);
+
     // 충전된 부분
-    HBRUSH brush = CreateSolidBrush(RGB(255, 0, 0)); // 홍색 브러시 생성
+    HBRUSH brush = CreateSolidBrush(RGB(255, 0, 0));
     HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
-    Rectangle(hdc, camera_x + 224, camera_y + 368, camera_x + 224 + filledWidth, camera_y + 368 + gaugeHeight);
+
+    Rectangle(hdc,
+        camera_x + 224, camera_y + 368,
+        camera_x + 224 + filledWidth, camera_y + 368 + gaugeHeight);
+
     SelectObject(hdc, oldBrush);
     DeleteObject(brush);
 }
 
 void Player::Render_SpeedGauge(HDC hdc, int camera_x, int camera_y)
 {
-    // 파워 게이지를 화면에 표시
-    int gaugeWidth = 265; // 게이지의 전체 너비
-    int gaugeHeight = 13; // 게이지의 높이
-    int filledWidth = static_cast<int>((Speed / Speed_Max) * gaugeWidth); // 충전된 게이지의 너비
+    const int gaugeWidth = 265;
+    const int gaugeHeight = 13;
+
+    int filledWidth = static_cast<int>((Speed / Speed_Max) * gaugeWidth);
 
     // 게이지 배경
-    Rectangle(hdc, camera_x + 224, camera_y + 383, camera_x + 224 + gaugeWidth, camera_y + 383 + gaugeHeight);
+    Rectangle(hdc,
+        camera_x + 224, camera_y + 383,
+        camera_x + 224 + gaugeWidth, camera_y + 383 + gaugeHeight);
+
     // 충전된 부분
-    HBRUSH brush = CreateSolidBrush(RGB(0, 0, 255)); // 파란색 브러시 생성
+    HBRUSH brush = CreateSolidBrush(RGB(0, 0, 255));
     HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
-    Rectangle(hdc, camera_x + 224, camera_y + 383, camera_x + 224 + filledWidth, camera_y + 383 + gaugeHeight);
+
+    Rectangle(hdc,
+        camera_x + 224, camera_y + 383,
+        camera_x + 224 + filledWidth, camera_y + 383 + gaugeHeight);
+
     SelectObject(hdc, oldBrush);
     DeleteObject(brush);
 }
 
 void Player::Render_HP(HDC hdc, int left, int top)
 {
-    // 파워 게이지를 화면에 표시
-    int gaugeWidth = 20; // 게이지의 전체 너비
-    int gaugeHeight = 5; // 게이지의 높이
-    int filledWidth = static_cast<int>((HP / HP_MAX) * gaugeWidth); // 충전된 게이지의 너비
+    const int gaugeWidth = 20;
+    const int gaugeHeight = 5;
+
+    int filledWidth = static_cast<int>((HP / HP_MAX) * gaugeWidth);
 
     // 게이지 배경
-    Rectangle(hdc, left, top + 22, left + gaugeWidth, top + 22 + gaugeHeight);
+    Rectangle(hdc,
+        left, top + 22,
+        left + gaugeWidth, top + 22 + gaugeHeight);
+
     // 충전된 부분
-    HBRUSH brush = CreateSolidBrush(RGB(0, 255, 0)); // 초록색 브러시 생성
+    HBRUSH brush = CreateSolidBrush(RGB(0, 255, 0));
     HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
-    Rectangle(hdc, left, top + 22, left + filledWidth, top + 22 + gaugeHeight);
+
+    Rectangle(hdc,
+        left, top + 22,
+        left + filledWidth, top + 22 + gaugeHeight);
+
     SelectObject(hdc, oldBrush);
     DeleteObject(brush);
 }
 
-#endif PLAYER_H
-
-// ---
-
-#ifndef LINE_H
-#define LINE_H
+// =========================== Line ===========================
 
 Line::Line()
-    :begin_x(0)
+    : begin_x(0)
     , begin_y(0)
     , end_x(0)
     , end_y(0)
     , angle(90.0)
-    , radian(0.0) {}
+    , radian(0.0)
+{
+}
 
 void Line::set_radian()
 {
-
+    // 현재는 사용 안 함 (각도는 toRadians로 변환)
 }
 
 void Line::Render_Line(HDC hdc, int camera_x, int camera_y)
@@ -231,23 +262,30 @@ void Line::Render_Line(HDC hdc, int camera_x, int camera_y)
     begin_x = 70;
     begin_y = 350;
 
-    double radianAngle = toRadians(angle + 90); // 선 그리기에서의 각도를 라디안으로 변환
+    double radianAngle = toRadians(angle + 90); // 각도를 라디안으로 변환
 
-    // 포물선 운동의 방향을 나타내는 선을 그립니다.
-    MoveToEx(hdc, camera_x+(int)begin_x, camera_y+(int)begin_y, nullptr);
-    LineTo(hdc, camera_x + begin_x + (30 * sin(radianAngle)), camera_y + begin_y + (30 * cos(radianAngle))); // Y좌표를 반대로 설정합니다.카메라에 맞춰 수정
+    MoveToEx(hdc,
+        camera_x + static_cast<int>(begin_x),
+        camera_y + static_cast<int>(begin_y),
+        nullptr);
+
+    LineTo(hdc,
+        camera_x + static_cast<int>(begin_x + (30 * sin(radianAngle))),
+        camera_y + static_cast<int>(begin_y + (30 * cos(radianAngle))));
 }
 
 void Line::Angle(bool isFire)
 {
-    if (isFire == true)
+    if (isFire)
         return;
+
     if (GetAsyncKeyState(VK_UP))
     {
         if (angle > 180)
             return;
         angle += 1;
     }
+
     if (GetAsyncKeyState(VK_DOWN))
     {
         if (angle < 0)
@@ -256,16 +294,11 @@ void Line::Angle(bool isFire)
     }
 }
 
-#endif LINE_H
-
-// ---
-
-#ifndef FIRE_H
-#define FIRE_H
+// =========================== Fire ===========================
 
 Fire::Fire()
-    :v_0(0)
-    , v_x(0)// 파워 값 0 ~ 30 정도가 적당해 보임
+    : v_0(0)
+    , v_x(0)      // 파워 값 0 ~ 30 정도
     , diameter(5.f)
     , gravity(10)
     , Time(0.1f)
@@ -274,13 +307,15 @@ Fire::Fire()
     , y(0)
     , isFire(false)
     , shoot_mode(0)
-    , shoot1(true) 
-    , shoot2(true) 
-    , shoot3(true) {}
+    , shoot1(true)
+    , shoot2(true)
+    , shoot3(true)
+{
+}
 
 void Fire::set_ball()
 {
-    if (isFire == false)
+    if (!isFire)
     {
         double radianAngle = toRadians(angle + 90);
         x = (left + 8.5) + (20 * sin(radianAngle));
@@ -290,21 +325,25 @@ void Fire::set_ball()
 
 void Fire::Render_Fire(HDC hdc)
 {
-    if (isSpaceUp == true)
+    // 발사 조건만 처리 (실제 그리기는 외부에서)
+    if (isSpaceUp)
         isFire = true;
 }
 
-void Fire::Action(bool* player_1turn, bool* player_2turn, double* ball_x, double* ball_y, int tank_mode)
+void Fire::Action(bool* player_1turn, bool* player_2turn,
+    double* ball_x, double* ball_y, int tank_mode)
 {
-    if (isFire == false)
+    if (!isFire)
     {
         v_x = power + 5;
     }
-    if (isFire == true)
+
+    if (isFire)
     {
         v_0 = power + 5;
         double radianAngle = toRadians(angle);
-        // 포물선 운동을 위한 x와 y 좌표 계산
+
+        // 탱크 타입에 따른 중력 값
         if (tank_mode == 1)
         {
             gravity = 10;
@@ -318,149 +357,161 @@ void Fire::Action(bool* player_1turn, bool* player_2turn, double* ball_x, double
             gravity = 15;
         }
 
-        x += v_0 * Time * cos(radianAngle) / 10; // 기존 위치에 더합니다.
-        y -= (v_x * Time * sin(radianAngle) - 0.5 * gravity * Time * Time) / 10; // 기존 위치에서 빼줍니다.
-        Time += 0.05; // 시간 증가
+        // 포물선 운동
+        x += v_0 * Time * cos(radianAngle) / 10;
+        y -= (v_x * Time * sin(radianAngle) - 0.5 * gravity * Time * Time) / 10;
+
+        Time += 0.05;
+
         *ball_x = x;
         *ball_y = y;
-        if (y > 800 || x > 1600 || x < 0) //    화면 밖 나가면 탄 사라짐
+
+        // 화면 밖으로 나가면 턴 종료
+        if (y > 800 || x > 1600 || x < 0)
         {
-            if (*player_1turn == true)
+            if (*player_1turn)
             {
                 *player_2turn = true;
                 *player_1turn = false;
             }
-            else if (*player_2turn == true)
+            else if (*player_2turn)
             {
                 *player_2turn = false;
                 *player_1turn = true;
             }
+
             Speed = 0;
             power = 0;
             Time = 1;
             power_now = 0;
-            isFire = false; isSpaceUp = false;
+            isFire = false;
+            isSpaceUp = false;
+
             set_ball();
             return;
         }
     }
 }
 
-void Fire::Hit(bool* player_1turn, bool* player_2turn, double left, double top, double* Player1_HP, double* Player2_HP, int tank_mode)
+void Fire::Hit(bool* player_1turn, bool* player_2turn,
+    double left, double top,
+    double* Player1_HP, double* Player2_HP,
+    int tank_mode)
 {
+    // 단순 박스 충돌
     if (left - 15 < x + 5 && top - 15 < y + 5 &&
         left + 35 > x && top + 35 > y)
     {
-        if (*player_1turn == true)
+        if (*player_1turn)
         {
             *player_2turn = true;
             *player_1turn = false;
-            if (shoot1 == true && shoot_mode == 1)
+
+            if (shoot1 && shoot_mode == 1)
             {
-                *Player2_HP = *Player2_HP - 50;
+                *Player2_HP -= 50;
                 shoot1 = false;
                 shoot_mode = 0;
             }
+
             if (tank_mode == 1)
             {
-                *Player2_HP = *Player2_HP - 15;
+                *Player2_HP -= 15;
             }
             else if (tank_mode == 2)
             {
-                *Player2_HP = *Player2_HP - 10;
+                *Player2_HP -= 10;
             }
             else if (tank_mode == 3)
             {
-                *Player2_HP = *Player2_HP - 20;
+                *Player2_HP -= 20;
             }
-            
         }
-        else if (*player_2turn == true)
+        else if (*player_2turn)
         {
             *player_2turn = false;
             *player_1turn = true;
-            if (shoot1 == true && shoot_mode == 1)
+
+            if (shoot1 && shoot_mode == 1)
             {
-                *Player1_HP = *Player1_HP - 50;
+                *Player1_HP -= 50;
                 shoot1 = false;
                 shoot_mode = 0;
             }
+
             if (tank_mode == 1)
             {
-                *Player1_HP = *Player1_HP - 15;
+                *Player1_HP -= 15;
             }
             else if (tank_mode == 2)
             {
-                *Player1_HP = *Player1_HP - 10;
+                *Player1_HP -= 10;
             }
             else if (tank_mode == 3)
             {
-                *Player1_HP = *Player1_HP - 20;
+                *Player1_HP -= 20;
             }
         }
+
         Speed = 0;
         power = 0;
         Time = 1;
         power_now = 0;
-        isFire = false; isSpaceUp = false;
+        isFire = false;
+        isSpaceUp = false;
+
         set_ball();
         return;
-
     }
 }
 
-void Fire::shoot_1(bool* player_1turn, bool* player_2turn, double* ball_x, double* ball_y)
+void Fire::shoot_1(bool* player_1turn, bool* player_2turn,
+    double* ball_x, double* ball_y)
 {
-    if (isFire == true)
+    if (isFire)
     {
         v_0 = power + 5;
         double radianAngle = toRadians(angle);
-        // 포물선 운동을 위한 x와 y 좌표 계산
-        x += v_0 * Time * cos(radianAngle) / 10; // 기존 위치에 더합니다.
-        y -= (v_0 * Time * sin(radianAngle) - 0.5 * gravity * Time * Time) / 10; // 기존 위치에서 빼줍니다.
-        Time += 0.05; // 시간 증가
+
+        x += v_0 * Time * cos(radianAngle) / 10;
+        y -= (v_0 * Time * sin(radianAngle) - 0.5 * gravity * Time * Time) / 10;
+
+        Time += 0.05;
+
         *ball_x = x;
         *ball_y = y;
-        /*double vx = speed * cos(radian);
-        double vy = speed * sin(radian);
-        const double windSpeed = 5.0;
-        double t = 0.0;
-        double dt = 0.1;
 
-        for (int i = 0; i < 1000; ++i) {
-            double x = (vx * t) + (0.5 * windSpeed * t * t);
-            double y = (vy * t) - (0.5 * gravity * t * t);
-
-            t += dt;
-        }
-        */
-        if (y > 840 || x > 1920 || x < 0) //    화면 밖 나가면 탄 사라짐
+        if (y > 840 || x > 1920 || x < 0)
         {
-            if (*player_1turn == true)
+            if (*player_1turn)
             {
                 *player_2turn = true;
                 *player_1turn = false;
             }
-            else if (*player_2turn == true)
+            else if (*player_2turn)
             {
                 *player_2turn = false;
                 *player_1turn = true;
             }
+
             Speed = 0;
             power = 0;
             Time = 1;
-            isFire = false; isSpaceUp = false;
+            isFire = false;
+            isSpaceUp = false;
+
             set_ball();
             return;
         }
     }
 }
 
-void Fire::shoot_2(bool* player_1turn, bool* player_2turn, double* ball_x, double* ball_y)
+void Fire::shoot_2(bool* player_1turn, bool* player_2turn,
+    double* ball_x, double* ball_y)
 {
     if (shoot_mode == 2)
     {
-        set_pos(*ball_x, *ball_y);
+        set_pos(static_cast<int>(*ball_x), static_cast<int>(*ball_y));
         shoot_mode = 0;
     }
 }
@@ -469,16 +520,18 @@ void Fire::shootmode()
 {
     if (GetAsyncKeyState(VK_F1))
     {
-        if (shoot1 == false)
+        if (!shoot1)
         {
             shoot_mode = 0;
             return;
         }
+
         if (shoot_mode == 1)
         {
             shoot_mode = 0;
             return;
         }
+
         shoot_mode = 1;
     }
     else if (GetAsyncKeyState(VK_F2))
@@ -488,6 +541,7 @@ void Fire::shootmode()
             shoot_mode = 0;
             return;
         }
+
         shoot_mode = 2;
     }
     else if (GetAsyncKeyState(VK_F3))
@@ -497,6 +551,7 @@ void Fire::shootmode()
             shoot_mode = 0;
             return;
         }
+
         shoot_mode = 3;
     }
 
@@ -512,5 +567,3 @@ void Fire::shootmode()
         }
     }
 }
-
-#endif FIRE_H
--- Client/Fortress/Player.h
+++ Client/Fortress/Player.h
@@ -1,96 +1,133 @@
 #pragma once
-#pragma once
+
 #include <Windows.h>
 #include <math.h>
 
+#ifndef PI
 #define PI 3.141519
+#endif
+
+// ========================== Player ===========================
 
 class Player
 {
 public:
-	double left;
-	double top;
-	double right;
-	double speed;
-	double angle;
-	int start_x;
-	int start_y;
-	double HP;
-	double HP_MAX;
-	double power;
-	int power_Max;
-	double Speed;
-	int Speed_Max;
-	bool isSpaceDown;
-	bool isSpacePress;
-	bool isSpaceUp;
-	bool isleftDown;
-	bool isleftPress;
-	bool isleftUp;
-	double power_now;
+    // 위치 및 이동
+    double left;
+    double top;
+    double right;
+    double speed;
+    double angle;
+    int    start_x;
+    int    start_y;
+
+    // 체력 / 파워 / 속도
+    double HP;
+    double HP_MAX;
+    double power;
+    int    power_Max;
+    double Speed;
+    int    Speed_Max;
+    double power_now;
+
+    // 입력 상태
+    bool isSpaceDown;
+    bool isSpacePress;
+    bool isSpaceUp;
+    bool isleftDown;
+    bool isleftPress;
+    bool isleftUp;
+
 public:
-	Player();
-	void Move(bool isFire, int tank_mode);
-	void Render(HDC hdc);
-	void set_pos(int p1_left, int p1_top);
-	void Charge();
-	void Space_Down();
-	void SpacePress();
-	void SpaceUp(HWND hWnd);
-	void Render_PowerGauge(HDC hdc,int camera_x,int camera_y);
-	void Update(bool isFire, HWND hWnd);
-	void Render_SpeedGauge(HDC hdc,  int camera_x, int camera_y);
-	void Render_HP(HDC hdc, int left, int top);
+    Player();
+
+    void Move(bool isFire, int tank_mode);
+    void Render(HDC hdc);
+    void set_pos(int p1_left, int p1_top);
+
+    void Charge();
+    void Space_Down();
+    void SpacePress();
+    void SpaceUp(HWND hWnd);
+
+    void Render_PowerGauge(HDC hdc, int camera_x, int camera_y);
+    void Render_SpeedGauge(HDC hdc, int camera_x, int camera_y);
+    void Render_HP(HDC hdc, int left, int top);
+
+    void Update(bool isFire, HWND hWnd);
 };
 
-// ---
+// =========================== Line ===========================
 
 class Line : public Player
 {
 public:
-	double begin_x;
-	double begin_y;
-	double end_x;
-	double end_y;
-	double angle;
-	double radian;
+    double begin_x;
+    double begin_y;
+    double end_x;
+    double end_y;
+
+    double angle;
+    double radian;
+
 public:
-	Line();
-	void set_radian();
-	void Render_Line(HDC hdc, int camera_x, int camera_y);
-	void Angle(bool isFire);
-	double toRadians(double degrees)
-	{
-		return degrees * (PI / 180.0);
-	}
+    Line();
+
+    void set_radian();
+    void Render_Line(HDC hdc, int camera_x, int camera_y);
+    void Angle(bool isFire);
+
+    double toRadians(double degrees)
+    {
+        return degrees * (PI / 180.0);
+    }
 };
 
-// ---
+// =========================== Fire ===========================
 
 class Fire : public Line
 {
 public:
-	int shoot_mode;
-	double diameter;
-	double v_0;
-	double v_x;
-	double gravity;
-	double Time;
-	double z;
-	double x;
-	double y;
-	bool isFire;
-	bool shoot1;
-	bool shoot2;
-	bool shoot3;
-	double toRadians(double degrees) { return degrees * 3.14159265358979323846 / 180.0; }
+    int    shoot_mode;
+
+    double diameter;
+    double v_0;
+    double v_x;
+    double gravity;
+    double Time;
+    double z;
+    double x;
+    double y;
+
+    bool   isFire;
+    bool   shoot1;
+    bool   shoot2;
+    bool   shoot3;
+
+    double toRadians(double degrees)
+    {
+        return degrees * 3.14159265358979323846 / 180.0;
+    }
+
 public:
-	Fire();
-	void set_ball();
-	void Render_Fire(HDC hdc);
-	void Action(bool* player_1turn, bool* player_2turn, double* ball_x, double* ball_y, int tank_mode);
-	void Hit(bool* player_1turn, bool* player_2turn, double left, double top, double* Player1_HP, double* Player2_HP, int tank_mode);
-	void shootmode();
-	void shoot_1(bool* player_1turn, bool* player_2turn, double* ball_x, double* ball_y);
-	void shoot_2(bool* player_1turn, bool* player_2turn, double* ball_x, double* ball_y);
+    Fire();
+
+    void set_ball();
+    void Render_Fire(HDC hdc);
+
+    void Action(bool* player_1turn, bool* player_2turn,
+        double* ball_x, double* ball_y, int tank_mode);
+
+    void Hit(bool* player_1turn, bool* player_2turn,
+        double left, double top,
+        double* Player1_HP, double* Player2_HP,
+        int tank_mode);
+
+    void shootmode();
+
+    void shoot_1(bool* player_1turn, bool* player_2turn,
+        double* ball_x, double* ball_y);
+
+    void shoot_2(bool* player_1turn, bool* player_2turn,
+        double* ball_x, double* ball_y);
 };
